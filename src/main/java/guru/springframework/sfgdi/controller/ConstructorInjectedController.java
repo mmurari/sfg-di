e0239715c251be970b2edb97743c05ed231aa9e1
murari <murarisgit@gmail.com>
2021-04-14 23:42:45 +1000

    - Add Qualifiers

--- src/main/java/guru/springframework/sfgdi/controller/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controller/ConstructorInjectedController.java
@@ -1,13 +1,14 @@
 package guru.springframework.sfgdi.controller;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ConstructorInjectedController {
     private final GreetingService service;
 
-    public ConstructorInjectedController(GreetingService service) {
+    public ConstructorInjectedController(@Qualifier("constructorInjectedService") GreetingService service) {
         this.service = service;
     }
 
---
+++ src/main/java/guru/springframework/sfgdi/services/SetterInjectedService.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SetterInjectedService implements GreetingService{
+
+    @Override
+    public String sayGreeting() {
+        return "hello world - setter";
+    }
+}
