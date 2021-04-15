e78b971a91d889ace8dfd37a23a03cec3fa11b71
murari <murarisgit@gmail.com>
2021-04-15 11:28:44 +1000

    - Primary Qualifier

--- src/main/java/guru/springframework/sfgdi/controller/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controller/MyController.java
@@ -1,11 +1,18 @@
 package guru.springframework.sfgdi.controller;
 
+import guru.springframework.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+    private final GreetingService service;
+
+    public MyController(GreetingService service) {
+        this.service = service;
+    }
+
     public String sayHello() {
-        System.out.println("Hello World!");
+        System.out.println(service.sayGreeting());
         return "Hi";
     }
 }
